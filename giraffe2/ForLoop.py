621bcd48a127943268ab8c639bf9eb31a33a59c7
Nazifagit config --glbal user.name Nazifagit clone(nazif@LAPTOP-0OF0NAIB MINGW64 ~$ cdnazif@LAPTOP-0OF0NAIB MINGW64 ~$ cd /cnazif@LAPTOP-0OF0NAIB MINGW64 /c$ ls'.Bin'/             OneDriveTemp/           Recovery/ Config.Msi/                PageFile.sys            swapfile.sys'Documents and Settings'@   PerfLogs/              'System Volume Information'/ hiberfil.sys              'Program Files'/         Users/ Intel/                    'Program Files (x86)'/   Windows/ OEM/                       ProgramData/nazif@LAPTOP-0OF0NAIB MINGW64 /c$ cd Gitbash: cd: Git: No such file or directorynazif@LAPTOP-0OF0NAIB MINGW64 /c$ cd gitbash: cd: git: No such file or directorynazif@LAPTOP-0OF0NAIB MINGW64 /c$ cd ProgramFilesbash: cd: ProgramFiles: No such file or directorynazif@LAPTOP-0OF0NAIB MINGW64 /c$ cd ProgramFiles/bash: cd: ProgramFiles/: No such file or directorynazif@LAPTOP-0OF0NAIB MINGW64 /c$ cd usersnazif@LAPTOP-0OF0NAIB MINGW64 /c/users$ cd nazifnazif@LAPTOP-0OF0NAIB MINGW64 /c/users/nazif$ cd PycharmProjectsnazif@LAPTOP-0OF0NAIB MINGW64 /c/users/nazif/PycharmProjects (master)$ cd giraffe2nazif@LAPTOP-0OF0NAIB MINGW64 /c/users/nazif/PycharmProjects/giraffe2 (master)$ ls bettercalculator.py      ForLoop.py     ifStatements.py        venv/ ComparisonOperators.py   functions.py  'Return Statement.py'   WhileLoop.py Dictionaries.py          Guessing.py    tuples.pynazif@LAPTOP-0OF0NAIB MINGW64 /c/users/nazif/PycharmProjects/giraffe2 (master)$ git config --global user.name Nazifa <nazifataha1@gmail.com>
2020-03-31 12:53:34 -0400

    Start ignoring idea

---
+++ giraffe2/ForLoop.py
@@ -0,0 +1,19 @@
+for letter in "Tiger Academy":
+    print(letter)
+friends = ["Jim", "Karen","Kevin"]
+for name in friends:
+    print(name)
+len(friends)
+for index in range(len(friends)):
+    print(friends[index])
+
+for index in range(10):
+    print(index)
+for index in range(3,10):
+    print(index)
+
+for index in range(5):
+    if index == 0:
+        print("first iteration")
+    else:
+        print("Not first")
